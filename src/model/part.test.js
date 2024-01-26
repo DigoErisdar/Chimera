45d16f2e6bae686d45542d44ddd8a97d41e4c1e7
Даниил Галкин В. <d.galkin@bars-open.ru>
2024-01-26 11:35:51 +0300

    add tests

--- src/model/part.test.js
+++ src/model/part.test.js
@@ -2,10 +2,19 @@ import { ComponentType, Head } from './part'
 
 class DummyHead extends Head {}
 
+class NewDummyHead extends DummyHead {}
+
 describe('Model Attrs', () => {
     test('check type', () => {
         expect(DummyHead.type).toBe(Head.type)
         expect(DummyHead.type).toBe(ComponentType.Head)
         expect(DummyHead.type === undefined).toBeFalsy()
+    })
+    test('check type with inherit', () => {
+        expect(NewDummyHead.type).toBe(Head.type)
+        expect(NewDummyHead.type).toBe(ComponentType.Head)
+    })
+    test('check type on instance', () => {
+        expect(new NewDummyHead().constructor.type).toBe(ComponentType.Head)
     })
 })
